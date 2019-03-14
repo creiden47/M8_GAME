c9db65b563e25c42af0575df67d3b6d1fbd40b68
Francisco Garcia Zillger <franciscogarzil@monlau.com>
2019-03-14 18:51:42 +0100

    Initial commit

---
+++ app/src/main/java/com/example/franciscogarzil/m8_game/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.franciscogarzil.m8_game;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+
+public class MainActivity extends AppCompatActivity {
+    String username = "";
+    String exp = "";
+    String lvl = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
