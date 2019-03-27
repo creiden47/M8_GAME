072a0b587845351cd1501899e965161f23bf2de5
ivanivamar <ivanivamar99@gmail.com>
2019-03-27 16:22:06 +0100

    More things

--- app/src/main/java/com/example/franciscogarzil/m8_game/CharacterPage.java
+++ app/src/main/java/com/example/franciscogarzil/m8_game/CharacterPage.java
@@ -1,29 +1,26 @@
 package com.example.franciscogarzil.m8_game;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class MainMenu extends Fragment {
-
-    public MainMenu() {
+public class CharacterPage extends Fragment {
+    public CharacterPage() {
         // Required empty public constructor
     }
-
-
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ((AppCompatActivity)getActivity()).getSupportActionBar().hide();
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.main_menu, container, false);
+        return inflater.inflate(R.layout.character_page, container, false);
     }
 }
