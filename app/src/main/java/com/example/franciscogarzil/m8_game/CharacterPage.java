03aa426aa0306fc2c8a547c90c0f1c0825a30edc
ivanivamar <ivanivamar99@gmail.com>
2019-03-28 18:52:20 +0100

    Status Increment Done. Functional Library

--- app/src/main/java/com/example/franciscogarzil/m8_game/CharacterPage.java
+++ app/src/main/java/com/example/franciscogarzil/m8_game/CharacterPage.java
@@ -6,38 +6,13 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-
-import static com.example.franciscogarzil.m8_game.MainMenu.currentCharacter;
 
 public class CharacterPage extends AppCompatActivity {
-
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.character_page);
-
-        // Text Views
-        // Welcome message
-        TextView welcomeMsg = findViewById(R.id.welcome_msg);
-        // Get Placeholder
-        String welcome_placeholder = getResources().getString(R.string.welcome) + " " + currentCharacter.get_name();
-        // Set Texts
-        welcomeMsg.setText(welcome_placeholder);
-        // Character Status
-        // Text Views
-        TextView _dmgStatus = findViewById(R.id.txtStatusDmg);
-        TextView _speedStatus = findViewById(R.id.txtStatusSpeed);
-        TextView _vitalityStatus = findViewById(R.id.txtStatusVitality);
-        TextView _masteryPoints = findViewById(R.id.masteryPoints);
-        // Get Placeholder
-        String _mpPlaceholder = getResources().getString(R.string.money) + " " + currentCharacter.get_masteryPoints();
-        // Set Texts
-        _masteryPoints.setText(_mpPlaceholder);
-        _dmgStatus.setText(currentCharacter.get_powerStat());
-        _speedStatus.setText(currentCharacter.get_asStat());
-        _vitalityStatus.setText(currentCharacter.get_hpStat());
-
+        new StatusInit(this);
         // Buttons
         Button _newGame = findViewById(R.id.btnNewGame);
         Button _library = findViewById(R.id.btnLibrary);
--- app/src/main/java/com/example/franciscogarzil/m8_game/storeFragment.java
+++ app/src/main/java/com/example/franciscogarzil/m8_game/storeFragment.java
@@ -1,6 +1,7 @@
 package com.example.franciscogarzil.m8_game;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -8,13 +9,72 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 
+import static com.example.franciscogarzil.m8_game.MainMenu.currentCharacter;
 
-public class storeFragment extends AppCompatActivity {
+public class storeFragment extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_store);
+
+        new StatusInit(this);
+
+        // Initialize Buttons:
+        ImageButton _goBack = findViewById(R.id.btnFkGoBack);
+        Button _incDmg = findViewById(R.id.btnIncrementDmg);
+        Button _incSpeed = findViewById(R.id.btnIncrementSpeed);
+        Button _incVitality = findViewById(R.id.btnIncrementVitality);
+        // On Click Listeners:
+        _goBack.setOnClickListener(this);
+        _incDmg.setOnClickListener(this);
+        _incSpeed.setOnClickListener(this);
+        _incVitality.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        // Current Character val:
+        Character currentCharacter;
+
+        // On Click Switch:
+        switch (v.getId()) {
+            case R.id.btnFkGoBack:
+                // Go Back to the CharacterPage:
+                Intent myIntent = new Intent(storeFragment.this, CharacterPage.class);
+                storeFragment.this.startActivity(myIntent);
+                break;
+            // Increment Buttons
+            case R.id.btnIncrementDmg:
+                // Damage:
+                try {
+                    currentCharacter = ConnectorSQL.getCharacter(MainMenu.currentCharacter.get_name());
+                    ConnectorSQL.incrementStat("power_stat", currentCharacter.get_name());
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+                break;
+            case R.id.btnIncrementSpeed:
+                // Speed:
+                try {
+                    currentCharacter = ConnectorSQL.getCharacter(MainMenu.currentCharacter.get_name());
+                    ConnectorSQL.incrementStat("as_stat", currentCharacter.get_name());
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+                break;
+            case R.id.btnIncrementVitality:
+                // Vitality:
+                try {
+                    currentCharacter = ConnectorSQL.getCharacter(MainMenu.currentCharacter.get_name());
+                    ConnectorSQL.incrementStat("hp_stat", currentCharacter.get_name());
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+                break;
+        }
     }
 }
