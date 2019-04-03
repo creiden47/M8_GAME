e1c9d2621f7cbc7babbf3dea538de4aaa8bf00db
creiden47 <franciscogarzil@monlau.es>
2019-04-03 17:35:30 +0200

    New character done

--- app/src/main/java/com/example/franciscogarzil/m8_game/PKG_ACTIVITIES/NewCharacter.java
+++ app/src/main/java/com/example/franciscogarzil/m8_game/PKG_ACTIVITIES/NewCharacter.java
@@ -1,16 +1,44 @@
 package com.example.franciscogarzil.m8_game.PKG_ACTIVITIES;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.franciscogarzil.m8_game.PKG_CLASS.Character;
+import com.example.franciscogarzil.m8_game.PKG_CONNECTION.ConnectorSQL;
 import com.example.franciscogarzil.m8_game.R;
 
 public class NewCharacter extends AppCompatActivity {
-
+    EditText EditTextNewCharacter = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.new_character);
 
+    }
+
+    public void createNewCharacter(View v) throws ClassNotFoundException {
+        try {
+            EditTextNewCharacter = (EditText) findViewById(R.id.EditTextName);
+            String newCharacter = EditTextNewCharacter.getText().toString();
+
+            ConnectorSQL.insertNewCharacter(newCharacter, Login.currentUserName);
+
+            Intent myIntent = new Intent(NewCharacter.this, MainMenu.class);
+            NewCharacter.this.startActivity(myIntent);
+        }catch(Exception e){
+            msToast("Error" + e);
+        }
+    }
+
+    public void msToast(String text) {
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 }
